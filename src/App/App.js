283f2f9098c1b1f34b2e6436e273d6ec607f32bd
Ron Martin <ronmartin89@gmail.com>
2020-03-09 07:54:51 -0700

    init commit

---
+++ src/App/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import './App.css';
+
+class App extends Component{
+
+  state = {};
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>Testing 123</div>
+        </header>
+      </div>
+    );
+  }
+
+}
+
+export default App;
