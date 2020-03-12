e3166be1b3d2dd582087239707f6bb492f8d7f1c
Ron Martin <ronmartin89@gmail.com>
2020-03-11 17:50:44 -0700

    Added conditional rendering

--- src/components/Spell/Spell.js
+++ src/components/Spell/Spell.js
@@ -5,7 +5,7 @@ export default function Spell(props) {
     return (
       <div className='Spell__main'>
           <h4>{props.name}</h4>
-          <p>{`Level: ${props.level} // School: ${props.school} // Range: ${props.range}`}</p>
+          <p>{`Level: ${props.level === 0 ? 'Cantrip': props.level} // School: ${props.school} // Range: ${props.range}`}</p>
           <p>{`Classes: ${props.classes} (FIX THIS: Currently using the class ID)`}</p>
           <p>{`Casting Time: ${props.castingTime} // Duration: ${props.duration} // Components: ${props.spellComponents}`}</p>
           <p>{`Description: ${props.description}`}</p>
@@ -16,18 +16,6 @@ export default function Spell(props) {
               <p>{props.higherLevels}</p>
             </>
           }
-
-          {/* key={spell.id}
-                name={spell.spell_name}
-                classes={spell.classes}
-                level={spell.level}
-                school={spell.school}
-                range={spell.range}
-                castingTime={spell.castingTime}
-                spellComponents={spell.components}
-                duration={spell.duration}
-                description={spell.description}
-                higherLevels={spell.higherLevels} */}
       </div>
     )
 }
