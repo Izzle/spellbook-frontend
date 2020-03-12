0d5a3cabf47ed8155f73610f987cf41217aa3358
Ron Martin <ronmartin89@gmail.com>
2020-03-12 12:33:20 -0700

    added comments to code

--- src/components/SpellBookButtons/SpellBookButtons.js
+++ src/components/SpellBookButtons/SpellBookButtons.js
@@ -1,12 +1,23 @@
 import React, { Component} from 'react';
+import { Link } from 'react-router-dom';
 import './SpellBookButtons.css';
 
 export default class SpellBookButtons extends Component {
 
+    // WE SHOULD DO CONDITIONAL RENDER
+    // OR EVEN A HIGHER ORDER COMPONENT
+    // LIKE IN THE ENDPOINTS SECTION
+    // In one case, load the button that lets you EDIT your spellbook
+    // in the other case, load the button that lets you SAVE your spellbook
+    // ALSO. The 2nd button shouldnt do anything when you are building your spellbook- 
+    //     so you don't accidentally click a link in the middle of your spellbook editing/building
+
     render() {
         return(<div>
-            <button>edit stuff</button>
-            <button>make this a Link back to the decks of cards!</button>
+            <button>edit spellbook</button>
+            <Link to='/spellbook' className='SpellBookButtons__Link'>
+                <button className='SpellBookButtons__button'>This button should tell you the name of the Spellbook you're in. Also, it will take you back to 'View Spellbook' page at /spellbook</button>
+            </Link>
         </div>);
     }
 }
