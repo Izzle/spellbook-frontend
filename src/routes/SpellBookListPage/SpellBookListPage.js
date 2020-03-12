484caf3eb0792a2bf137a44ca3fc3861976a63f3
Ron Martin <ronmartin89@gmail.com>
2020-03-12 11:14:47 -0700

    Made static SpellBookListPage with basic css

--- src/routes/SpellBookListPage/SpellBookListPage.js
+++ src/routes/SpellBookListPage/SpellBookListPage.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './SpellBookListPage.css';
 
 export default class SpellBookListPage extends Component {
   render() {
     return (
       <section className='SpellBookListPage'>
+        <div className='SpellBookListPage__container'>
           <h2>View your Spellbook selections here!</h2>
-          {/* I'm hardcoding in the :id here, but it should take the ID from the spellbook from the database when we have user accounts*/}
+          {/* I'm hardcoding in the :id here, but it should take the ID from the spellbook from the DATABASE when we have user accounts*/}
           {/* <Link to='/spellbook/:id' className='SpellBookListPage__Link'></Link> */}
           <Link to='/spellbook/1' className='SpellBookListPage__Link'>
-            Deck 1 (should be)
+            <button className='SpellBookListPage__button'>Deck 1</button>
+          </Link>
+          <Link to='/spellbook/2' className='SpellBookListPage__Link'>
+            <button className='SpellBookListPage__button'>Deck 2</button>
+          </Link>
+          <Link to='/spellbook/3' className='SpellBookListPage__Link'>
+            <button className='SpellBookListPage__button'>Deck 3</button>
           </Link>
           <Link to='/spellbook/:id' className='SpellBookListPage__Link'>
-            Deck 2
+            <button className='SpellBookListPage__button'>Change decks to use the :id params from the Database!</button>
           </Link>
-          <Link to='/spellbook/:id' className='SpellBookListPage__Link'>
-            Deck 3
-          </Link>
+        </div>
       </section>
     )
   }
