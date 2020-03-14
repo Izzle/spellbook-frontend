2c7da40d5f38228152e692326d31307696aa92a6
Ron Martin <ronmartin89@gmail.com>
2020-03-13 21:41:19 -0700

    Cleaned up code and moved CSS files to the new component

--- src/routes/SpellBookListPage/SpellBookListPage.js
+++ src/routes/SpellBookListPage/SpellBookListPage.js
@@ -29,20 +29,6 @@ export default class SpellBookListPage extends Component {
                       id={spellbook.id}
                       name={spellbook.spellbook_name}/>
           })}
-          {/* I'm hardcoding in the :id here, but it should take the ID from the spellbook from the DATABASE when we have user accounts*/}
-          {/* <Link to='/spellbook/:id' className='SpellBookListPage__Link'></Link> */}
-          <Link to='/spellbook/1' className='SpellBookListPage__Link'>
-            <button className='SpellBookListPage__button'>Deck 1</button>
-          </Link>
-          <Link to='/spellbook/2' className='SpellBookListPage__Link'>
-            <button className='SpellBookListPage__button'>Deck 2</button>
-          </Link>
-          <Link to='/spellbook/3' className='SpellBookListPage__Link'>
-            <button className='SpellBookListPage__button'>Deck 3</button>
-          </Link>
-          <Link to='/spellbook/:id' className='SpellBookListPage__Link'>
-            <button className='SpellBookListPage__button'>Change decks to use the :id params from the Database!</button>
-          </Link>
         </div>
       </section>
     )
