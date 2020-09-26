32af6dccdd8ece0480ca750227f248f28612474b
Ron Martin <ronmartin89@gmail.com>
2020-09-25 20:56:33 -0700

    styling the spellbook buttons to look like actual books

--- src/components/SpellBookLink/SpellBookLink.js
+++ src/components/SpellBookLink/SpellBookLink.js
@@ -5,8 +5,6 @@ import './SpellBookLink.css';
 export default function SpellBookLink(props){
 
     return(
-        
-      //<Link to={`/spellbook/${props.id}`} className='SpellBookLink__Link'>
         <Link to={{
             pathname: `/spellbook/${props.id}`,
             state: {
@@ -14,6 +12,7 @@ export default function SpellBookLink(props){
             }}} 
             className='SpellBookLink__Link'>
           <button className='SpellBookLink__button'>{props.name}</button>
+          <p>MAKE THE LINKS LOOK LIKE ACTUAL BOOKS?!</p>
         </Link>
     );
 }
